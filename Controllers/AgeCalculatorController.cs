0dfac2e336f66ef0b579b04592a99aae15d28e28
Ammar <ammar.hakanovic@outlook.com>
2022-03-03 09:28:00 +0100

    initial upload

---
+++ Controllers/AgeCalculatorController.cs
@@ -0,0 +1,29 @@
+﻿using AgeCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgeCalculator.Controllers
+{
+    public class AgeCalculatorController : Controller
+    {
+        // GET: AgeCalculator
+        public ActionResult Index()
+        {
+            //get datapicker
+            AgeModel model = new AgeModel();
+            model.DOB = DateTime.Now;
+            return View(model);
+        }
+
+
+        public ActionResult Div()
+        {
+            ViewBag.Message = "Centriranje div elementra";
+
+            return View();
+        }
+    }
+}
---
+++ Models/AgeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AgeCalculator.Models
+{
+    public class AgeModel
+    {
+        [DataType(DataType.Date)]
+       // [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DOB { get; set; }
+        public int Age { get; set; }
+        public int Months { get; set; }
+        public int Day { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Secunde { get; set; }
+
+    }
+}
